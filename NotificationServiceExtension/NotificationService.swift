ba5760515aeeb37f577b79cf56ea7100dd97ee32
Aryzae <a.aryzae@live.jp>
2018-06-01 19:09:35 +0900

    NotificationService Extension

---
+++ NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by Sho Ito on 2018/06/01.
+//  Copyright © 2018年 Aryzae. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        let content = request.content.userInfo["gcm.notification.image_url"]
+        print("\(request.content.userInfo)")
+        print("\(content ?? "nil")")
+
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
